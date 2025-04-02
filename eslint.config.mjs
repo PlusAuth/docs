22764925624ed559636907e7b5af9030f1165c60
Ayaz Reis <ayazismailhakki@gmail.com>
2025-04-02 14:55:32 +0300

    refactor: use markdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
-import * as mdx from 'eslint-plugin-mdx'
+import * as mdx from "eslint-plugin-mdx"
 
 export default [
   {
-    ignores: [
-      "content/en/tools/ldap-proxy/index.mdx"
-    ]
+    ignores: ["content/en/tools/ldap-proxy/index.mdx"]
   },
   {
     ...mdx.flat,
@@ -13,16 +11,16 @@ export default [
       lintCodeBlocks: false,
       // optional, if you want to disable language mapper, set it to `false`
       // if you want to override the default language mapper inside, you can provide your own
-      languageMapper: {},
-    }),
+      languageMapper: {}
+    })
   },
   {
     ...mdx.flatCodeBlocks,
     rules: {
       ...mdx.flatCodeBlocks.rules,
       // if you want to override some rules for code blocks
-      'no-var': 'error',
-      'prefer-const': 'error',
-    },
-  },
+      "no-var": "error",
+      "prefer-const": "error"
+    }
+  }
 ]
